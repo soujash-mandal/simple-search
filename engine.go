66d060bc78ea60bf88c18361862d10f763acfdf7
Soujash Mandal <2002soujash@gmail.com>
2026-06-06 19:04:05 +0530

    add document

--- engine.go
+++ engine.go
@@ -11,3 +11,25 @@ func NewSearchEngine() *SearchEngine {
 		Index:     make(map[string]map[int]struct{}),
 	}
 }
+
+func (s *SearchEngine) AddDocument(doc Document) {
+	// Store the document
+	s.Documents[doc.ID] = doc
+
+	// Tokenize title and content
+	titleTokens := tokenize(doc.Title)
+	contentTokens := tokenize(doc.Content)
+
+	// Combine all tokens
+	tokens := append(titleTokens, contentTokens...)
+
+	// Build inverted index
+	for _, token := range tokens {
+		// Create a set for the token if it doesn't exist
+		if _, exists := s.Index[token]; !exists {
+			s.Index[token] = make(map[int]struct{})
+		}
+		// Add document ID to the token's posting list
+		s.Index[token][doc.ID] = struct{}{}
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
 func main() {
+	engine := NewSearchEngine()
 
+	engine.AddDocument(Document{
+		ID:      1,
+		Title:   "Redis Guide",
+		Content: "Redis is an in memory database",
+	})
+
+	engine.AddDocument(Document{
+		ID:      2,
+		Title:   "MongoDB Basics",
+		Content: "MongoDB is a document database",
+	})
 }
